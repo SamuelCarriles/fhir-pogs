a622ffc87a86c14f308ff2bb5af961b40d898eb7
SamuelCarriles <samuelcarrilesbanos@gmail.com>
2025-07-11 18:24:08 -0400

    create-resource fn added

--- src/fhir_pogs/core.clj
+++ src/fhir_pogs/core.clj
@@ -1,1 +1,6 @@
-(ns fhir-pogs.core)
+(ns fhir-pogs.core
+  (:require [cheshire.core :refer [parse-string]]))
+
+(defn parse-resource "Parse a json resource to a clojure map."
+  [json]
+  (parse-string json true))
