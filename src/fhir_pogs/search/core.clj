8db6bd8b88e9f1abd3ae8a71c06a3b441f935421
SamuelCarriles <samuelcarrilesbanos@gmail.com>
2025-08-01 19:09:42 -0400

    Modifiers support begined, namespaces fhir-pogs.search.core and fhir-pogs.search.modifiers created

--- src/fhir_pogs/search/core.clj
+++ src/fhir_pogs/search/core.clj
@@ -1,23 +1,22 @@
-(ns fhir-pogs.search
+(ns fhir-pogs.search.core
   (:require [clojure.string :as str]
             [honey.sql :as sql]
             [honey.sql.helpers :as h]
             [fhir-pogs.db :as db]
             [fhir-pogs.core :refer [search-resources!]]
             [cheshire.core :refer [parse-string generate-string]]
-            [next.jdbc :as jdbc]
             [fhir-search.uri-query :refer [parse]]))
 
 (def search-params (:entry (parse-string (slurp "resources/search-parameters.json") true)))
 (def compartment-definitions (:entry (parse-string (slurp "resources/compartment-definitions.json") true)))
 
-
 (defn param-path [restype param]
-  (let [paths (map #(get-in % [:resource :expression]) (filter #(= param (get-in % [:resource :code])) search-params))] 
+  (let [paths (map #(get-in % [:resource :expression]) (filter #(= param (get-in % [:resource :code])) search-params))
+        parse-where #(if-not (nil? %) (re-find #"^.*?(?=\.where.*)|^.*$" %) nil)]
     (->> (map #(re-find (re-pattern (str "(?<=" restype "\\.).*")) %) (mapcat #(str/split % #" \| ") paths))
          (remove nil?)
          first
-         (re-find #"^.*?(?=\.where.*)|^.*$"))))
+         parse-where)))
 
 (defn compartment-paths [type compartment]
   (when-let [refres (:type compartment)]
@@ -28,12 +27,6 @@
          (filter #(= (:code %) type))
          (mapcat :param)
          (map #(str % ".reference")))))
-
-(defn gen-comparator
-  ([v]
-   (str " ? (@ == \"" v "\")"))
-  ([k v]
-   (str k v)))
 
 (defn jsonb-paths-exists [path comp]
   (when-not (or (nil? path) (not (seq path)) (nil? comp) (not (string? comp)) (str/blank? comp))
@@ -50,19 +43,23 @@
       (let [op (-> m name keyword)
             sv (str "\"" v "\"")
             values (str/split v #"\|")
-            comp  (case op
-                    :exact (str " ? (@ ==" sv ")")
-                    :contains (str " ? (@ like_regex \".*" v ".*\")")
-                    :missing ""
-                    :not (str " ? (@ !=" sv ")")
-                    :text (str " ? (@ like_regex \".*" v ".*\")")
-                    :in (reduce #(str %1 "@ == \"" %2 "\"" (if (= %2 (last values)) ")" " || ")) " ? (" values)
-                    :not-in (reduce #(str %1 "@ != \"" %2 "\"" (if (= %2 (last values)) ")" " && ")) " ? (" values)
-                    :identifier (str " ? (@.system ==" (first values) "&& @.value ==" (second values) ")")
-                    :code-text (str " ? (@ like %" v "%)")
-                    :iterate (str " ? (@ ==" sv ")")
-                    :of-type (str " ? (@.type == " sv ")")
-                    (throw (IllegalArgumentException. "Incorrect modifier.")))]
+            comp (case op
+                   :exact (str " ? (@ ==" sv ")")
+                   :contains (str " ? (@ like_regex \".*" v ".*\")")
+                   :missing ""
+                   :not (str " ? (@ !=" sv ")")
+                   :text (str " ? (@ like_regex \".*" v ".*\")")
+
+                   :in (reduce #(str %1 "@ == \"" %2 "\"" (if (= %2 (last values)) ")" " || ")) " ? (" values)
+                   :not-in (reduce #(str %1 "@ != \"" %2 "\"" (if (= %2 (last values)) ")" " && ")) " ? (" values)
+
+                   :identifier (str " ? (@.system ==" (first values) "&& @.value ==" (second values) ")")
+                   :code-text (str " ? (@ like %" v "%)")
+                   :iterate (str " ? (@ ==" sv ")")
+                   :of-type (str " ? (@.type == " sv ")")
+                   :above (str " ? (@ starts with " sv ")")
+                   :below (str " ? (" sv " starts with @)") ;;Revisar soporte para below
+                   (throw (IllegalArgumentException. (str "Incorrect modifier: " m))))]
         (jsonb-paths-exists path comp))))
 ;;ajustar esto para cuando sea :above, :below, :text-advanced
 
@@ -74,7 +71,7 @@
                               (and name value (nil? params))
                               (gen-param-cond (param-path restype name) modifier value)
                               ;;
-                              (and name params (nil? value)) (str "params work" "bro")
+                              (and name params (nil? value)) "Deam bro"
                               :else nil)) params)]
       (if (> (count params) 1) (vec (conj conditions :and)) (first conditions)))))
 
@@ -88,33 +85,34 @@
         compartment (:compartment ast)
         compartment-cond (jsonb-paths-exists
                           (compartment-paths type compartment)
-                          (gen-comparator (str (:type compartment) "/" (:id compartment))))
+                          (str " ? (@ == \"" (:type compartment) "/" (:id compartment) "\")"))
         params-cond (gen-params-cond type join params)]
-    (-> [[:= :resourceType type]]
-        (conj (when id [:= :resource-id id]) compartment-cond params-cond))))
+    (conj [[:= :resourceType type]] (when id [:= :resource-id id]) compartment-cond params-cond)))
 
 (defn search-fhir! [db-spec table-prefix uri]
   (let [ast (parse uri)
         conditions (gen-cond-clauses ast)]
-    (search-resources! db-spec table-prefix (:type ast) conditions))) 
+    (search-resources! db-spec table-prefix (:type ast) conditions)))
 
 (comment
   ;; Esta es la forma de buscar en jsonb: [:jsonb_path_exists :content [:cast "$.**.given.**? (@ == \"Isabel\" )" :jsonpath]]
-  
+
   ;;Tenemos que saber que las clausulas condicionales van a tener este formato:
   ;; [:operador :campo :valor]
-  
+
   (def db-spec {:dbtype "postgresql"
                 :dbname "resources"
                 :host "localhost"
                 :user "postgres"
                 :port "5432"
-                :password "postgres"}) 
-  
-  (search-fhir! db-spec "testing" "/Patient/example/ClinicalAssessment?finding-code:exact=850.0")
+                :password "postgres"})
+
+  (search-fhir! db-spec "testing" "/Patient/example/ClinicalAssessment?finding-code:below=850.00")
   (search-fhir! db-spec "testing" "/Patient?given=Juan&family=Pérez")
- 
-  :.
-  )
+  (search-fhir! db-spec "testing" "/Questionnaire?publisher:text=FHIR%20Infrastructure")
+
+
+
+  :.)
 
 
---
+++ src/fhir_pogs/search/modifiers.clj
@@ -0,0 +1,61 @@
+(ns fhir-pogs.search.modifiers
+  (:require [clj-http.client :as http]
+            [cheshire.core :as json]
+            [clojure.string :as str]))
+
+(def ^:private terminology-server "https://tx.fhir.org/r4")
+
+(defn expand-valueset [valueset-url]
+  (try
+    (let [response (http/get (str terminology-server "/ValueSet/$expand")
+                             {:query-params {:url valueset-url}
+                              :accept :json
+                              :as :json
+                              :timeout 10000})]
+      (:body response))
+    (catch Exception e (println (str "Error: " (.getMessage e)))
+           nil)))
+
+(expand-valueset "http://hl7.org/fhir/ValueSet/administrative-gender")
+
+
+
+(defn get-operator [m v]
+  (let [op (when-not (nil? m) (-> m name keyword))
+        vl (str/split v #"\|")
+        value (if (re-find #"\|" v) 
+                (str "(@.system == \"" (first vl) "\" && @.code == \"" (second vl) "\"") 
+                v)
+        sv (str "\"" value "\"") 
+        operator (case op
+                   :exact (str " ? (@ ==" sv ")")
+                   :contains (str " ? (@ like_regex \".*" v ".*\")")
+                   :missing ""
+                   :not (str " ? (@ !=" sv ")") 
+
+                   :in (reduce #(str %1 "@ == \"" %2 "\"" (if (= %2 (last value)) ")" " || ")) " ? (" value)
+                   :not-in (reduce #(str %1 "@ != \"" %2 "\"" (if (= %2 (last value)) ")" " && ")) " ? (" value)
+
+                   :identifier (str " ? (@.system ==" (first value) "&& @.value ==" (second value) ")")
+                   :code-text (str " ? (@ like %" v "%)")
+                   :iterate (str " ? (@ ==" sv ")")
+                   :of-type (str " ? (@.type == " sv ")")
+                   :above (str " ? (@ starts with " sv ")")
+                   :below (str " ? (" sv " starts with @)") ;;Revisar soporte para below
+                   nil (str "? (@ ==\"" v "\")")
+                   (throw (IllegalArgumentException. (str "Incorrect modifier: " m))))]
+                   [value operator]))
+
+(defn g-o [m v]
+  (let [[part1 part2] (str/split v #"\|")
+        mod (when m (-> m name keyword))]
+    (if mod
+      (case mod
+        :exact (str " ? (@ == \"" part1 "\")")
+        :in "Hay que extraer las cosas del value set")
+      (if part2
+        (str " ? (@.system == " part1 " && @.code == " part2 ")")
+        (str "? (@ == \"" v "\")")))))
+
+(g-o nil "http://loinc.org|88765")
+
