5959f5d16d0642ca4027edc7f5f0ad926612ac6c
SamuelCarriles <samuelcarrilesbanos@gmail.com>
2025-09-02 15:06:31 -0400

    db namespace was updated

--- src/fhir_pogs/db.clj
+++ src/fhir_pogs/db.clj
@@ -1,54 +1,114 @@
 (ns fhir-pogs.db
   (:require [next.jdbc :as jdbc]
             [honey.sql.helpers :as help]
-            [honey.sql :as sql]))
+            [honey.sql :as sql]
+            [clojure.set :as set]))
+;;Execution functions
+(defn execute!
+  "Execute a SQL statement against the database."
+  [db-spec sql-statement]
+  {:pre [(some? db-spec) (vector? sql-statement)]}
+  (try
+    (jdbc/execute! db-spec sql-statement)
+    (catch Exception e
+      (throw (ex-info "Database execution failed"
+                      {:db-spec db-spec
+                       :sql sql-statement
+                       :error (.getMessage e)})))))
 
-(defn jdbc-execute! "Obtains the datasource corresponding to the `db-spec`, connects to the database, and executes a `jdbc/execute!` within a `with-open` block."
-  [db-spec sentence]
-  (let [my-datasource (jdbc/get-datasource db-spec)]
-    (with-open [connection (jdbc/get-connection my-datasource)]
-      (jdbc/execute! connection sentence))))
+(defn execute-one!
+  "Execute a SQL statement and return only the first result."
+  [db-spec sql-statement]
+  {:pre [(some? db-spec) (vector? sql-statement)]}
+  (try
+    (jdbc/execute-one! db-spec sql-statement)
+    (catch Exception e
+      (throw (ex-info "Database execution failed"
+                      {:db-spec db-spec
+                       :sql sql-statement
+                       :error (.getMessage e)})))))
 
-(defn jdbc-transaction! [db-spec sentences]
-  (jdbc/with-transaction [tx (jdbc/get-datasource db-spec)]
-    (mapv #(jdbc/execute-one! tx %) sentences)))
+(defn transact!
+  "Execute multiple SQL statements in a transaction.
+   All statements succeed or all fail."
+  [db-spec sql-statements]
+  {:pre [(some? db-spec) (sequential? sql-statements) (every? vector? sql-statements)]}
+  (try
+    (jdbc/with-transaction [tx db-spec]
+      (mapv #(jdbc/execute-one! tx %) sql-statements))
+    (catch Exception e
+      (throw (ex-info "Transaction failed"
+                      {:db-spec db-spec
+                       :statements sql-statements
+                       :error (.getMessage e)})))))
 
-(defn get-tables! "Retorna una seq de keywords que son las tablas que existen en una base de datos especificada."
+;; Database introspection functions
+(defn get-tables
+  "Returns a set of table names that exist in the public schema."
   [db-spec]
-  (->> (map #(:tables/table_name %)
-            (jdbc-execute! db-spec (-> (help/select :table-name)
-                                       (help/from :information-schema.tables)
-                                       (help/where [:= :table-schema "public"]
-                                                   [:= :table-type "BASE TABLE"])
-                                       sql/format)))
-       (map keyword)
-       set))
+  (try
+    (->> (execute! db-spec
+                   (-> (help/select :table-name)
+                       (help/from :information-schema.tables)
+                       (help/where [:= :table-schema "public"]
+                                   [:= :table-type "BASE TABLE"])
+                       sql/format))
+         (map :tables/table_name)
+         (map keyword)
+         set)
+    (catch Exception e
+      (throw (ex-info "Failed to get tables" {:db-spec db-spec :error (.getMessage e)})))))
 
-(defn get-columns-of! [db-spec table-name]
-  (->> (jdbc-execute! db-spec (-> (help/select :column-name)
-                                  (help/from :information-schema.columns)
-                                  (help/where [:= :table-schema "public"]
-                                              [:= :table-name (name table-name)])
-                                  (help/order-by :ordinal-position)
-                                  sql/format))
-       (mapcat (fn [col]
-                 (map (fn [[_ v]] (keyword v)) col)))
-       set))
+(defn get-columns
+  "Returns a set of column names for the specified table."
+  [db-spec table-name]
+  {:pre [(some? table-name)]}
+  (try
+    (->> (execute! db-spec
+                   (-> (help/select :column-name)
+                       (help/from :information-schema.columns)
+                       (help/where [:= :table-schema "public"]
+                                   [:= :table-name (name table-name)])
+                       (help/order-by :ordinal-position)
+                       sql/format))
+         (map :columns/column_name)
+         (map keyword)
+         set)
+    (catch Exception e
+      (throw (ex-info "Failed to get columns"
+                      {:db-spec db-spec
+                       :table-name table-name
+                       :error (.getMessage e)})))))
 
-(defn table-remove! "Borra una o varias tablas en una base de datos especificada."
-  [db-spec n]
-  (if (and (= 1 (count n)) (= :all (first n)))
-    (let [sentence
-          (->> (get-tables! db-spec)
-               (apply help/drop-table)
-               sql/format)]
-      (if (seq (get-tables! db-spec)) (jdbc-execute! db-spec sentence) nil))
-    (when (and (seq (get-tables! db-spec))
-               (every? true?
-                       (reduce (fn [o i]
-                                 (if (some #(= % i) n)
-                                   (conj o true)
-                                   (conj o false)))
-                               #{} (get-tables! db-spec))))
-      (jdbc-execute! db-spec (-> (apply help/drop-table n)
-                                 sql/format)))))
+;; Table management functions
+(defn drop-tables!
+  "Drop one or more tables from the database."
+  [db-spec table-names]
+  {:pre [(some? db-spec) (sequential? table-names) (every? some? table-names)]}
+  (let [existing-tables (get-tables db-spec)
+        tables-to-drop (if (= [:all] table-names)
+                         existing-tables
+                         (set (map keyword table-names)))
+        missing-tables (set/difference tables-to-drop existing-tables)]
+
+    (when (seq missing-tables)
+      (throw (ex-info "Some tables don't exist"
+                      {:missing-tables missing-tables
+                       :existing-tables existing-tables})))
+
+    (when (seq tables-to-drop)
+      (execute! db-spec
+                (-> (apply help/drop-table tables-to-drop)
+                    sql/format)))))
+
+(defn table-exists?
+  "Check if a table exists in the database."
+  [db-spec table-name]
+  (contains? (get-tables db-spec) (keyword table-name)))
+
+(defn tables-exist?
+  "Check if all specified tables exist in the database."
+  [db-spec table-names]
+  (let [existing-tables (get-tables db-spec)
+        requested-tables (set (map keyword table-names))]
+    (set/subset? requested-tables existing-tables)))
